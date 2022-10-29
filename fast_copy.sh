fc481d72b6539f6d8842cf8dd2cec08b9080d361
iglad <schwanzuslongus23@proton.me>
2022-10-29 18:48:16 +0000

    added color support and change

--- fast_copy.sh
+++ fast_copy.sh
@@ -2,19 +2,29 @@
 #!/bin/bash
 
 
+# get colors
+. $(dirname "$0")/colors.sh
+
+
 TARGET_DIR=$(find -L ~ -type d | fzf)
 
-cp $1 $TARGET_DIR
-
-cd $TARGET_DIR
-
-
-ls -f
-
 if [[ -f $1 ]]; then
-    echo -e "\n\n### $1 copy succedded ### \n\n"
+    cp $1 $TARGET_DIR
+elif [[ -d $1 ]]; then
+    cp -r $1 $TARGET_DIR
 else
-    echo -e "\n\n### could not copy $1 ### \n\n"
+    echo -e "\n\n${Red}### cannot copy $1 ( src is not a file or dir ) ### ${Color_Off}\n\n"
+    exit 1
+fi
+
+
+
+ls -allt $TARGET_DIR | less
+
+if [[ -f $1 ]] || [[ -d $1 ]]; then
+    echo -e "${Green}\n\n### $1 copy succedded ### ${Color_Off}\n\n"
+else
+    echo -e "${Red}\n\n### could not copy $1 ### ${Color_Off}\n\n"
 fi
 
 
